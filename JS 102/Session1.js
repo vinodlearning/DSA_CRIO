4183dbab37156f6d468b8b73c790e8cc8f911bd6
vinodlearning <vinodlearningkey@gmail.com>
2024-10-29 23:53:16 +0530

    change

--- JS 102/Session1.js
+++ JS 102/Session1.js
@@ -1,1 +1,23 @@
-console.log("Session 1------>");
+console.log("Session 1---bbb--->");
+
+
+function twoSumInSortedArray(n, arr, target_sum) {
+    console.log("twoSumInSortedArray---------------")
+    let i=0;
+    let j=n-1;
+    while(i<=j){
+        let sum=arr[i]+arr[j];
+            if(sum==target_sum){
+                return true;
+            }else if(sum>target_sum){
+                j--;
+            }else {
+                i--;
+            }
+
+
+    }
+    return false;
+}
+
+console.log(twoSumInSortedArray(5,[2 ,4 ,5 ,8 ,9],7));
