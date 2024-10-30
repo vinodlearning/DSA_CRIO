ec6759070c393efb2dc28027a60f925784ed2051
vinodlearning <vinodlearningkey@gmail.com>
2024-10-30 23:47:24 +0530

    change

--- JS 102/Session1.js
+++ JS 102/Session1.js
@@ -21,4 +21,30 @@ function twoSumInSortedArray(n, arr, target_sum) {
 }
 let arr=[1 ,8 ,12 ,14 ,20, 24 ,29 ,30 ,32 ,41 ,44, 46, 47, 48 ,49, 50, 54, 55, 57, 59, 60, 61 ,63, 68 ,69, 70 ,71, 74 ,75 ,76 ,79 ,81 ,83 ,88, 90 ,92, 94 ,96];
 
-console.log(twoSumInSortedArray(38,arr,16));
+//console.log(twoSumInSortedArray(38,arr,16));
+
+// Time complexity is O(nlongn--> bcz of sort)
+function twoSum(nums, target) {
+    let duplicateArr=[...nums];
+    let left=0;
+    let right=nums.length-1;
+    nums.sort((a,b)=>a-b);
+    while(left<right){
+        const sum=nums[left]+nums[right];
+        if(sum==target){
+            break;
+        }else if(sum>target){
+            right--;
+        }else{
+            left++;
+        }
+    }
+
+    let firtsIdx=duplicateArr.indexOf(nums[left]);
+    let secondIdx=duplicateArr.indexOf(nums[right]);
+
+    return [firtsIdx,secondIdx];
+
+}
+
+console.log(twoSum([2, 4, 5, 9, 8],7));
